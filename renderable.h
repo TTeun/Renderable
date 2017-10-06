71bf3a14f243c49b7a55c699c49b602668379a9b
TTeun <teunverstraaten@gmail.com>
2017-10-06 18:38:07 +0200

    Renderable is now abstract class

--- renderable.h
+++ renderable.h
@@ -18,39 +18,16 @@ public:
   Renderable();
   ~Renderable();
 
+  virtual void createBuffers(QOpenGLFunctions_4_1_Core *ui) = 0;
+  virtual void createShader(QObject *ui)                    = 0;
+  virtual void render(QOpenGLFunctions_4_1_Core *ui, QMatrix4x4 &projectionMatrix) = 0;
+
 protected:
-  friend class RenderList;
   QOpenGLShaderProgram *m_shaderProgram;
   GLint m_projecionMatrixUniform;
   GLint m_modelViewMatrixUniform;
-  GLint m_readNormalsUniform;
-  QMatrix4x4 m_modelViewMatrix;
-
-  unique_ptr<QVector<QVector3D>> m_vertices;
-  unique_ptr<QVector<QVector4D>> m_colors;
-  unique_ptr<QVector<QVector3D>> m_normals;
-  unique_ptr<QVector<unsigned int>> m_indices;
 
   uint m_vao;
-  uint m_coordsBO;
-  uint m_colourBO;
-  uint m_normalBO;
-  uint m_indicesBO;
-
-  GLint m_frame = 0;
-
-  void createBuffers(QOpenGLFunctions_4_1_Core *ui);
-  void updateBuffers(QOpenGLFunctions_4_1_Core *ui);
-  void createShader(QObject *ui);
-  virtual void render(QOpenGLFunctions_4_1_Core *ui, QMatrix4x4 &projectionMatrix);
-  void load_obj(const char *filename);
-
-  struct State
-  {
-    GLenum polygonMode = GL_FILL;
-    bool readNormals   = false;
-  };
-  State m_state;
 };
 
 #endif // RENDERABLE_H
--- surfacerenderable.h
+++ surfacerenderable.h
@@ -7,6 +7,36 @@ class SurfaceRenderable : public Renderable
 {
 public:
   SurfaceRenderable();
+  ~SurfaceRenderable();
+
+  void createBuffers(QOpenGLFunctions_4_1_Core *ui) override;
+  void createShader(QObject *ui) override;
+  void render(QOpenGLFunctions_4_1_Core *ui, QMatrix4x4 &projectionMatrix) override;
+
+protected:
+  GLint m_readNormalsUniform;
+  QMatrix4x4 m_modelViewMatrix;
+
+  unique_ptr<QVector<QVector3D>> m_vertices;
+  unique_ptr<QVector<QVector4D>> m_colors;
+  unique_ptr<QVector<QVector3D>> m_normals;
+  unique_ptr<QVector<unsigned int>> m_indices;
+
+  void updateBuffers(QOpenGLFunctions_4_1_Core *ui);
+  void load_obj(const char *filename);
+
+  uint m_coordsBO;
+  uint m_colourBO;
+  uint m_normalBO;
+  uint m_indicesBO;
+
+  struct State
+  {
+    GLenum polygonMode = GL_FILL;
+    bool readNormals   = false;
+  };
+  State m_state;
+  GLint m_frame = 0;
 };
 
 #endif // SURFACERENDERABLE_H
